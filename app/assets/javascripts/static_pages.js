76f90269ed306e3595f5c0dc32dbcb3ce92256cb
AzDarGee <ashishdarji88@gmail.com>
2015-07-01 19:47:38 -0400

    updated styling and added modal

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,7 +1,5 @@
-var clock = document.getElementById("timer");
-var targetDate = new Date(2015, 08, 01);
- 
-  clock.innerHTML = countdown(targetDate).toString();
-  setInterval(function(){
-    clock.innerHTML = countdown(targetDate).toString();
-  }, 1000);
+$(document).on("page:change", function() {
+
+
+});
+
