f503fb0713c2f878ac4fb42323700aa48c460c10
AzDarGee <ashishdarji88@gmail.com>
2015-09-05 19:10:44 -0400

    changed ruby version

--- db/migrate/20150905222721_add_fields_to_static_pages.rb
+++ db/migrate/20150905222721_add_fields_to_static_pages.rb
@@ -1,12 +1,6 @@
 class AddFieldsToStaticPages < ActiveRecord::Migration
   def change
-  	# drop_table :addresses
-  	# drop_table :comments
-  	# drop_table :devapps
-  	# drop_table :sites
-  	# drop_table :static_pages
-  	# drop_table :statuses
-  	# drop_table :users
+
   	create_table :static_pages do |t|
       t.string :email
       t.string :name
